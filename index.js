ac5c7c7f88eecf48d793aeef7748914ca8be4ee1
IC3CLD <67324553+IC3CLD@users.noreply.github.com>
2020-09-03 15:07:40 +0300

    first hw

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 "use strict";
 
-const name = "Генератор защитного поля";
-const price = 1000;
+// const name = `"Генератор защитного поля"`;
+// let price = 1000;
 
-console.log(`Выбран`, name, "");
+// console.log(`Выбран ${ name}, цена за штуку ${price}, кредитов`);
+
+// price = 2000;
+
+// console.log(`Выбран ${ name}, цена за штуку ${price}, кредитов`);
+
+const total = 100;
+const order = 20;
+console.log(total>order);
