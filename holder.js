193f7f8aeb9375f2f40abbf7ee572533d8b8850c
IC3CLD <67324553+IC3CLD@users.noreply.github.com>
2020-09-05 10:11:41 +0300

    Задачки

---
+++ holder.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// const name = `"Генератор защитного поля"`;
+// let price = 1000;
+
+// console.log(`Выбран ${ name}, цена за штуку ${price}, кредитов`);
+
+// price = 2000;
+
+// console.log(`Выбран ${ name}, цена за штуку ${price}, кредитов`);
+
+// let total = 100;
+// const order = 130;
+
+// if (total > order) {
+//   total = `Заказ оформлен, с вами свяжется менеджер`;
+// }
+// if (total < order) {
+//     total = `На складе недостаточно твоаров!`;
+//   }
+
+// console.log(total);
--- index.js
+++ index.js
@@ -1,14 +1,177 @@
 "use strict";
 
-// const name = `"Генератор защитного поля"`;
-// let price = 1000;
+// let day = prompt(`Введите день недели`);
+//  switch (day) {
+//     case "monday":
+//     case "tuesday":
+//     case "thirsday":
+//     case "thursday":
+//     case "friday":
+//      console.log("Это будний день");
+//      break;
 
-// console.log(`Выбран ${ name}, цена за штуку ${price}, кредитов`);
+//      case "saturday":
+//      case "sunday":
+//         console.log("Это выходной");
+//         break;
+     
+//      default:
+//          console.log("Enter valid day");
+//  }
 
-// price = 2000;
+// if (day === "monday" ||day ==="tuesday" || day ==="thirsday" || day ==="thursday" || day ==="friday") {console.log("Это будний день")}
+//  else if (day === "saturday" || day ==="sunday") {console.log("Это выходной")}
+//  else {console.log("Enter valid day")};
 
-// console.log(`Выбран ${ name}, цена за штуку ${price}, кредитов`);
+// let b = prompt(`date`);
+// let c = b%4;
+// if (c===2) {
+//     alert(`Высокостный год`);
+// } else {
+//     alert(`Не высокотный`)
+// }
 
-const total = 100;
-const order = 20;
-console.log(total>order);
+
+// let myAge = 25;
+// let rightNeibor = 23 ;
+// let result = myAge + rightNeibor;
+// console.log(result);
+
+// let circleRadius = 10;
+// console.log(Math.PI*(Math.pow(circleRadius, 2)));
+
+// let me = `Valerii`;
+// let age = 25;
+// let camp = `bootcamp №24`;
+// console.log(`Меня зовут ${ me}, мне ${age} лет, я учусь в ${ camp}.`);
+
+
+// for (let i = 0; i < 10; i++) {
+//     i % 2 === 0 ? console.log(i) : '';
+// }
+
+// if (i % 2 === 0 && i !== 0) {
+//     console.log(i);
+//     break;
+// }
+
+// for(let i = 0; i<10; ) {
+//      console.log(`i is:`, i++);
+//  }
+ 
+//  for(let A = 0; A<10; ) {
+//     console.log(`A is:`, ++A);
+// }
+// let q;
+
+// while (q !== null && q !== ``) {
+//     q = prompt(`enter yours number: `);q
+//     console.log(q);
+// }
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// if (true) {
+//     c = 4;
+// }
+
+// console.log(c);
+
+// let PASSWORD = `qwerty`;
+
+// let prompt1 = prompt(`Enter your name: `);
+
+// if (prompt1) {
+//     prompt1 === PASSWORD
+//     ? alert(`Password accept`) 
+//     : alert(`Incorrect password`);
+// } else {
+//     alert(`Cancel`);
+// };
+
+// let vova = {
+//     age : 18,
+//     dad : "ne Valakas"
+// }
+// vova.age >= 18 || vova.dad === `Valakas` 
+// ?  alert(`Na pitsu` ) 
+// : alert(`Ne dam pitsu! AHAHAHAH`);
+
+// if(vova.age >= 18) {
+//     alert(`Na pitsu`);
+// } else if (vova.dad === `Valakas`) {
+//     alert(`Na pitsu`);
+// } else {
+//     alert(`Ne dam pitsu! AHAHAHAH`);
+// }
+
+// if(vova.age >= 18 || vova.dad === `Valakas`) {
+//     alert(`Na pitsu`);
+// } else {
+//     alert(`Ne dam pitsu! AHAHAHAH`);
+// }
+
+
+
+
+// const name = `Valeri`;
+// let age = prompt(`Введите в возраст`);
+// console.log(typeof age);
+// console.log(age);
+// // age = +age
+// age = Number(age)
+// console.log(typeof age);
+
+// let answer = confirm(`Ты хочешь питсы?`);
+// console.log(typeof answer);
+// console.log(answer);
+// let question = `Вы любите JS?`;
+// let newAnswer = confirm(question);
+// console.log(newAnswer);
+// console.log(typeof newAnswer);
+
+// let num = 10;
+// let boolNum = Boolean(num);
+// console.log(boolNum);
+// num = 0;
+// boolNum = Boolean(num);
+// console.log(boolNum);
+// let str = `Sasha`;
+// let boolStr = Boolean(str);
+// console.log(boolStr);
+// str = ``;
+// boolStr = Boolean(str);
+// console.log(boolStr);
+// str = ` `;
+// boolStr = Boolean(str);
+// console.log(boolStr);
+
+// let isOpen;
+// console.log(isOpen);
+// console.log(typeof isOpen);
+
+// let boolUnd = Boolean(isOpen);
+// console.log(boolUnd);
+// console.log(typeof boolUnd);
+
+// isOpen = null;
+// console.log(isOpen);
+// console.log(typeof isOpen);
+
+// let boolNull = Boolean(isOpen);
+// console.log(boolNull);
+// console.log(typeof boolNull);
+
+
+
+// const message = "Hello " + name;
+// console.log(message);
+// let newMassage = `My age is ${age}`;
+// console.log(newMassage);
+
+// age = 50;
+// console.log(newMassage);
+// console.log(age);
